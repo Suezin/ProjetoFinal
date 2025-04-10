127801335baf59a1a47fe0f71138f01a2d348aff
Suezin <matheusbaumhardt654@gmail.com>
2025-04-09 22:22:00 -0300

    first commit

---
+++ src/controller/GerenciadorConexao.java
@@ -0,0 +1,13 @@
+package controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class GerenciadorConexao {
+    
+}
