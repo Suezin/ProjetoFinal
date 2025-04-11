59925d1a1e685318de5409a670afd105818ce408
Suezin <matheusbaumhardt654@gmail.com>
2025-04-10 22:17:01 -0300

    add funcoes de cadastro

---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import model.Usuario;
+
+public class UsuarioController {
+    public boolean autenticar(String email, String senha) {
+        String sql = "SELECT * FROM usuarios " + "WHERE email = ? AND senha = ? ";
+
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        PreparedStatement comando = null;
+        ResultSet resultado = null;
+        try {
+            comando = gerenciador.prepararConexao(sql);
+
+            comando.setString(1, email);
+            comando.setString(2, senha);
+            resultado = comando.executeQuery();
+            if (resultado.next()) {
+                return true;
+
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            gerenciador.fecharConexao(comando, resultado);
+
+        }
+        return false;
+    }
+    
+//    public boolean inserirUsuario(Usuario usu) {
+//        String sql = "INSERT INTO usuarios(nome, email, senha, datanasc) " + "VALUES (?,?,?,?)";
+//        GerenciadorConexao gerenciador = new GerenciadorConexao();
+//        PreparedStatement comando = null;
+//        try {
+//            comando = gerenciador.prepararConexao(sql);
+//            comando.setString(1, usu.getNome());
+//            comando.setString(2, usu.getEmail());
+//            comando.setString(3, usu.getSenha());
+//            comando.setDate(4, new java.sql.Date(usu.getDatanasc().getTime()));
+//            comando.executeUpdate();
+//            return true;
+//        } catch (SQLException e) {
+//            JOptionPane.showMessageDialog(null, e.getMessage());
+//
+//        } finally {
+//            gerenciador.fecharConexao();
+//        }
+//        return false;
+//    }
+}
